ca905af5018bea8e8ee41a44dbda92f963e8e16b
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-06-26 16:09:36 -0500

    Removed certain folders

--- python/Algorithms/BubbleSort/bubbleSort.py
+++
@@ -1,8 +0,0 @@
-def bubbleSort(arr):
-    for i in range(len(arr)):
-        for j in range(0, len(arr) - i - 1):
-            if(arr[j] > arr[j+1]):
-                arr[j], arr[j+1] =  arr[j+1], arr[j]
-    return arr
-
-print(bubbleSort([5,3,1,6,2]))

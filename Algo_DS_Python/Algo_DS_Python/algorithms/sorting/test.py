15cceea64b118ad1f675e8b8a6366519e57a9407
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-06-26 18:55:51 -0500

    Added unitTests for sorting algorithms

--- Algo_DS_Python/Algo_DS_Python/algorithms/sorting/test.py
+++ Algo_DS_Python/Algo_DS_Python/algorithms/sorting/test.py
@@ -1,13 +1,28 @@
-from Sorting import BubbleSort
-from Sorting import InsertionSort
-from Sorting import MergeSort
-from Sorting import SelectionSort
+from bubbleSort import bubbleSort
+from insertionSort import insertionSort
+from binaryInsertionSort import binaryInsertionSort
+from mergeSort import mergeSort
+from selectionSort import selectionSort
 
 import unittest
 
 class test(unittest.TestCase): 
+    arr = [5,12,8,18,16,19,6,13,2,10,20,15,7,9,14,3,11,1,17,4]
+    sortedArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     def test_BubbleSort(self):
-        self.assertEqual(1,1)
+        self.assertEqual(bubbleSort.bubbleSort(test.arr),test.sortedArr)
+
+    def test_insertionSort(self):
+        self.assertEqual(insertionSort.insertionSort(test.arr),test.sortedArr)
+
+    def test_binaryInsertionSort(self):
+        self.assertEqual(binaryInsertionSort.binaryInsertionSort(test.arr),test.sortedArr)
+
+    def test_mergeSort(self):
+        self.assertEqual(mergeSort.mergeSort(test.arr),test.sortedArr)
+
+    def test_selectionSort(self):
+        self.assertEqual(selectionSort.selectionSort(test.arr),test.sortedArr)
 
 if __name__ == '__main__': 
     unittest.main()

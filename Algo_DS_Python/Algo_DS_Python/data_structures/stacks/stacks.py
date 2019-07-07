d375c82c01a7eca56d656dc91a902ccff595ed62
Abdullahi Farah <abdullahi.farah96@gmail.com>
2019-07-06 20:10:37 -0500

    Stacks (#8)
    * added __init__.py file to stack folder
    
    * Renamed stack folder to stacks
    
    * added __init__.py
    
    * Removed file
    
    * Implemented a array stack in python
    
    * Added gitignore file
    
    * Update LinkedList.py (#6)
    
    * Revert "Update LinkedList.py (#6)" (#7)
    
    This reverts commit 8f8a3144ec47cadd1b8cb7f87c9ca86a16a3aad2.

---
+++ Algo_DS_Python/Algo_DS_Python/data_structures/stacks/stacks.py
@@ -0,0 +1,59 @@
+#Only for use in Python 2.6.0a2 and later
+from __future__ import print_function
+import sys
+sys.path.append( '.' )
+sys.path.append( '../linkedList' )
+
+from linkedList import LinkedList
+from linkedList import Node
+
+class ArrayStack:
+    def __init__(self, data=None):
+        self.stack = [None] * 10 if data is None else [data] + [None] * 9
+        self.size = 1 if data is not None else 0
+        self.top = 0 if data is not None else -1
+
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        return False
+
+    def peek(self):
+        if self.isEmpty():
+            print("Stack is empty")
+            return None
+        return self.stack[self.top]
+    
+    def push(self, x):
+        self.top += 1
+        if self.top > len(self.stack)-1:
+            self.resize()
+        self.stack[self.top] = x
+        self.size += 1
+        self.printStack()
+
+    def pop(self):
+        if self.isEmpty():
+            print("Stack is empty")
+            return None
+        self.top -= 1
+        self.size -= 1
+        self.printStack()
+        return self.stack[self.top+1]
+    
+    def printStack(self):
+        print("Stack is:", end=" ")
+        for i in range(self.size):
+            print(self.stack[i], end=" ")
+        print("")
+
+    def resize(self):
+        newArr = [None] * (len(self.stack)*2)
+        for i in range(0, len(self.stack)):
+            newArr[i] = self.stack[i]
+        self.stack = newArr
+
+
+
+
+

93e5cb84319bd058833cbc4d11319276701f53f2
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-07-25 15:47:23 -0500

    Cleaned up imports

--- Algo_DS_Python/Algo_DS_Python/algorithms/sorting/test.py
+++ Algo_DS_Python/Algo_DS_Python/algorithms/sorting/test.py
@@ -1,13 +1,13 @@
-from bubbleSort import bubbleSort
-from insertionSort import insertionSort
-from binaryInsertionSort import binaryInsertionSort
-from mergeSort import mergeSort
-from selectionSort import selectionSort
-from quickSort import quickSort
+from bubbleSort.bubbleSort import bubbleSort
+from insertionSort.insertionSort import insertionSort
+from binaryInsertionSort.binaryInsertionSort import binaryInsertionSort
+from mergeSort.mergeSort import mergeSort
+from selectionSort.selectionSort import selectionSort
+from quickSort.quickSort import quickSort
 from quickSort.quickSort import randomized_quickSort
-from countingSort import countingSort
+from countingSort.countingSort import countingSort
 from countingSort.countingSort import shiftedCountingSort
-from radixSort import radixSort
+from radixSort.radixSort import radixSort
 
 import unittest
 
@@ -15,34 +15,34 @@ class test(unittest.TestCase):
     arr = [5,12,8,18,16,19,6,13,2,10,20,15,7,9,14,3,11,1,17,4]
     sortedArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     def test_BubbleSort(self):
-        self.assertEqual(bubbleSort.bubbleSort(test.arr),test.sortedArr)
+        self.assertEqual(bubbleSort(test.arr),test.sortedArr)
 
     def test_insertionSort(self):
-        self.assertEqual(insertionSort.insertionSort(test.arr),test.sortedArr)
+        self.assertEqual(insertionSort(test.arr),test.sortedArr)
 
     def test_binaryInsertionSort(self):
-        self.assertEqual(binaryInsertionSort.binaryInsertionSort(test.arr),test.sortedArr)
+        self.assertEqual(binaryInsertionSort(test.arr),test.sortedArr)
 
     def test_mergeSort(self):
-        self.assertEqual(mergeSort.mergeSort(test.arr),test.sortedArr)
+        self.assertEqual(mergeSort(test.arr),test.sortedArr)
 
     def test_selectionSort(self):
-        self.assertEqual(selectionSort.selectionSort(test.arr),test.sortedArr)
+        self.assertEqual(selectionSort(test.arr),test.sortedArr)
 
     def test_quickSort(self):
-        self.assertEqual(quickSort.quickSort(test.arr, 0, len(test.arr) - 1),test.sortedArr)
+        self.assertEqual(quickSort(test.arr, 0, len(test.arr) - 1),test.sortedArr)
 
     def test_randomized_quickSort(self):
-        self.assertEqual(quickSort.randomized_quickSort(test.arr, 0, len(test.arr) - 1),test.sortedArr)
+        self.assertEqual(randomized_quickSort(test.arr, 0, len(test.arr) - 1),test.sortedArr)
 
     def test_countingSort(self):
-        self.assertEqual(countingSort.countingSort(test.arr, 21),test.sortedArr)
+        self.assertEqual(countingSort(test.arr, 21),test.sortedArr)
 
     def test_shiftedCountingSort(self):
-        self.assertEqual(countingSort.shiftedCountingSort(test.arr, 20),test.sortedArr)
+        self.assertEqual(shiftedCountingSort(test.arr, 20),test.sortedArr)
 
     def test_radixSort(self):
-        self.assertEqual(radixSort.radixSort(test.arr, 20),test.sortedArr)
+        self.assertEqual(radixSort(test.arr, 20),test.sortedArr)
 
 if __name__ == '__main__': 
     unittest.main()

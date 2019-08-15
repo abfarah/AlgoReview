63dfdf36f0aa42318470dadbb6bb77e601e9c943
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-08-15 18:46:00 -0500

    Implemented memoization in  generating fibonacci sequence in python.

---
+++ Algo_DS_Python/Algo_DS_Python/algorithms/DynamicProgramming/fibonacci/fibonacci.py
@@ -0,0 +1,21 @@
+memo = {}
+def fib(n):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        f = 1
+    else:
+        f = fib(n-1) + fib(n-2)
+        memo[n] = f
+    return f
+
+def bottomUpFib(n):
+    fib = {}
+    for k in range(1, n+1):
+        if k <= 2:
+            f = 1
+        else:
+            f = fib[k-1] + fib[k-2]
+        fib[k] = f
+    return fib[n]
+

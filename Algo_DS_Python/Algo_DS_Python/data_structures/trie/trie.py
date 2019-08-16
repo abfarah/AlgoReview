afa4b5c3f33a2ba69b733750edd024e2b81a1b5b
Abdullahi Farah <abdullahi.farah96@gmail.com>
2019-08-16 15:02:28 -0500

    Trie (#35)
    * Initialized files for trie data structure
    
    * Implemented the trie data structure using python

---
+++ Algo_DS_Python/Algo_DS_Python/data_structures/trie/trie.py
@@ -0,0 +1,43 @@
+#Only for use in Python 2.6.0a2 and later
+from __future__ import print_function
+
+class Node:
+    def __init__(self):
+        self.children = [None]*26
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def letterPosition(self, l):
+        return ord(l) - 97
+
+    def insert(self, word):
+        word = word.lower()
+        n = len(word)
+        temp = self.root
+        for i in range(n):
+            index = self.letterPosition(word[i])
+            if temp.children[index] == None:
+                temp.children[index] = Node()
+            temp = temp.children[index]
+        temp.isEnd = True
+
+    def search(self, word):
+        word = word.lower()
+        n = len(word)
+        temp = self.root
+        for i in range(n):
+            index = self.letterPosition(word[i])
+            if temp.children[index] == None:
+                print("Nothing Found")
+                return False
+            temp = temp.children[index]
+        if temp.isEnd == True:
+            print("word Found")
+            return True
+        else:
+            print("Nothing Found")
+            return False
+

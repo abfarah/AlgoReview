1f971291e2ba9740e42dc1bd908fe2f66ab25b01
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-08-16 13:15:53 -0500

    Initialized files for dijkstra algorithm solution

---
+++ Algo_DS_Python/Algo_DS_Python/algorithms/graphs/dijkstra/dijkstra.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+import sys
+import os
+dirpath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(".")
+sys.path.append(dirpath + "/../../../data_structures/")
